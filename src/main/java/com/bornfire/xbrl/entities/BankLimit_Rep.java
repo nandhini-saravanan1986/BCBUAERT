25f6aac130e5917f32630f6fbccc2d2ad873210b
Rajalakshmi <rajalakshmi.r@bornfire.co.in>
2025-12-15 15:51:30 +0530

    ASL Alignement changes Update

--- src/main/java/com/bornfire/xbrl/entities/BankLimit_Rep.java
+++ src/main/java/com/bornfire/xbrl/entities/BankLimit_Rep.java
@@ -9,7 +9,12 @@ import org.springframework.data.repository.query.Param;
 
 public interface BankLimit_Rep extends CrudRepository<BankLimit_Entity,String>{
 	
-	@Query(value = "SELECT * FROM MIS_BANK_LIMITS WHERE REPORT_DATE = :reportDate ORDER BY S_NO", nativeQuery = true)
+	@Query(
+			  value = "SELECT * FROM MIS_BANK_LIMITS " +
+			          "WHERE TRUNC(REPORT_DATE) = TRUNC(:reportDate) " +
+			          "ORDER BY S_NO",
+			  nativeQuery = true
+			)
 	List<BankLimit_Entity> getalllist(@Param("reportDate") Date reportDate);
 
 	@Query(value = "select * from MIS_BANK_LIMITS WHERE REPORT_DATE=?1 ORDER BY BANK_NAME desc", nativeQuery = true)
