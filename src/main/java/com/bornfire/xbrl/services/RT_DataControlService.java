da44e7d4bf12d81247b7111181f2590f5ef4c987
kjeffrin <136725753+kjeffrin@users.noreply.github.com>
2026-02-18 17:32:38 +0530

    Update RT_DataControlService.java

--- src/main/java/com/bornfire/xbrl/services/RT_DataControlService.java
+++ src/main/java/com/bornfire/xbrl/services/RT_DataControlService.java
@@ -1,5 +1,9 @@
 package com.bornfire.xbrl.services;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 
@@ -36,10 +40,33 @@ public class RT_DataControlService {
 	            dto.setDelFlg("N");       // Not deleted
 	            dto.setModifyFlg("N");    // Not modified
 	            dto.setReport_name(report_name);
+				LocalDate localDate = dto.getPortfolioDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+	            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+				String DateString = localDate.format(formatter); 
+	            RT_DataControl data= RT_DatacontrolRepository.getdata(DateString,report_name);
+	            if(data != null && !data.equals(null)) {
+	            	System.out.println("Service: createOrUpdate() called  ---  Existing");
+	            	data.setContactEmployee1Email1(dto.getContactEmployee1Email1());
+	            	data.setContactEmployee1Name1(dto.getContactEmployee1Name1());
+	            	data.setContactEmployee1Title1(dto.getContactEmployee1Title1());
+	            	data.setContactEmployee2Email2(dto.getContactEmployee2Email2());
+	            	data.setContactEmployee2Name2(dto.getContactEmployee2Name2());
+	            	data.setContactEmployee2Title2(dto.getContactEmployee2Title2());
+	            	data.setSignOffCfoEmail(dto.getSignOffCfoEmail());
+	            	data.setSignOffCfoName(dto.getSignOffCfoName());
+	            	data.setSignOffCroEmail(dto.getSignOffCroEmail());
+	            	data.setSignOffCroName(dto.getSignOffCroName());
+	            	data.setSignOffHeadOfTreasuryName(dto.getSignOffHeadOfTreasuryName());
+	            	data.setSignOffHeadOfTreasuryEmail(dto.getSignOffHeadOfTreasuryEmail());
+	            	RT_DatacontrolRepository.save(data);
+	            }
+	            else {
+	            	System.out.println("Service: createOrUpdate() called  ---  New");
+	            	RT_DatacontrolRepository.save(dto);
+	            }
 	            
 	        }
 
-	        RT_DatacontrolRepository.save(dto);
 	        return "Saved successfully.";
 	    } catch (Exception e) {
 	        e.printStackTrace();
